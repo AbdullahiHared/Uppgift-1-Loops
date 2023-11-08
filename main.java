515388891cccf8222f1bfa81bdce8d3a42354345
Abdullahi <muxsinxareed6@gmail.com>
2023-11-08 10:19:44 +0100

    Skriv ut tal 2 till 5

--- main.java
+++ main.java
@@ -11,6 +11,16 @@ public class main {
        System.out.println(tal);
       }
 
+      sc.nextLine();
+
+      // b
+
+      int tal2 = 5;
+
+      for (int i = 2; i <= tal2; i++) {
+        System.out.println(i);
+      }
+
      
 
 
