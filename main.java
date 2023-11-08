c46c0dca3c4576100c7f2ba23c6aac43b22ac34d
Abdullahi <muxsinxareed6@gmail.com>
2023-11-08 10:13:13 +0100

    Skriv ut tal från 1 till 5

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner (System.in);
+      // a 
+      int tal = 0;
+
+      while (tal < 5) {
+       tal++;
+       System.out.println(tal);
+      }
+
+     
+
+
+
+    }
+}
